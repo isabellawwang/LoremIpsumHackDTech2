a3aa4596785be7802c424c9200739a5b3684eac1
bilgetatar <bt96@duke.edu>
2020-07-19 18:01:21 +0300

    randomly generated prompts done

--- LoremIpsum/PromptViewController.swift
+++ LoremIpsum/PromptViewController.swift
@@ -12,20 +12,35 @@ class PromptViewController: UIViewController {
 
     @IBOutlet weak var prompt: UILabel!
     
+    @IBAction func buttonClicked(_ sender: Any) {
+        let randomInt = Int.random(in: 1..<5)
+        if (randomInt==1) {
+            prompt.text = "Recognize your stereotypic responses within society, label them, and replace them with non-stereotypic responses."
+        } else if (randomInt == 2) {
+            prompt.text = "Imagine an example of a member of an out-group who counters popularly held stereotypes."
+        }else if (randomInt == 3) {
+            prompt.text = "View individuals according to their personal, rather than stereotypical characteristics"
+        }else if (randomInt == 4) {
+            prompt.text = "Adopt the perspective in the first person of a member of a stigmatized group."
+        }else if (randomInt == 5) {
+            prompt.text = "Have you recently attempted to increase your exposure to out-group members?"
+        }
+    }
     
     override func viewDidLoad() {
         let randomInt = Int.random(in: 1..<5)
         if (randomInt==1) {
-            prompt.text = ""
+            prompt.text = "Recognize your stereotypic responses within society, label them, and replace them with non-stereotypic responses."
         } else if (randomInt == 2) {
-            prompt.text = ""
+            prompt.text = "Imagine an example of a member of an out-group who counters popularly held stereotypes."
         }else if (randomInt == 3) {
-            prompt.text = ""
+            prompt.text = "View individuals according to their personal, rather than stereotypical characteristics"
         }else if (randomInt == 4) {
-            prompt.text = ""
+            prompt.text = "Adopt the perspective in the first person of a member of a stigmatized group."
         }else if (randomInt == 5) {
-            prompt.text = ""
+            prompt.text = "Have you recently attempted to increase your exposure to out-group members?"
         }
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.

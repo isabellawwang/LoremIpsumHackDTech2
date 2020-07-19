cffdbe0f73497a12a6127e45dc2b5979227fecc5
bilgetatar <bt96@duke.edu>
2020-07-19 17:05:11 +0300

    small glitches solved

--- LoremIpsum/AboutViewController.swift
+++ LoremIpsum/AboutViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class AboutViewController: UIViewController {
 
     override func viewDidLoad() {
+        self.navigationItem.hidesBackButton = true;
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.

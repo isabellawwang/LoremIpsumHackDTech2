533d5cc8f8287755aa59ae1c4a0493bfaf52c384
bilgetatar <bt96@duke.edu>
2020-07-19 11:56:47 +0300

    opening page added

---
+++ LoremIpsum/IntroViewController.swift
@@ -0,0 +1,29 @@
+//
+//  IntroViewController.swift
+//  LoremIpsum
+//
+//  Created by Bilge Tatar on 7/19/20.
+//  Copyright © 2020 Bilge Tatar. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
